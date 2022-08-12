2b623c6b93dcfe4a4a2deb5436e99c8aada4c463
joshua.grefte <joshua.grefte@kainos.com>
2022-08-12 08:47:52 +0100

    first iteration of popdensity_model.pkl and related create function notebook

--- helpers.py
+++ helpers.py
@@ -64,3 +64,17 @@ def so2_function(lat, lon):
     #predict with model
     preds = so2_model.predict(input)
     return preds[0]
+
+def popdensity_function(lat, lon):
+    #preprocessing, convert lat/lon to radians
+    df = pd.DataFrame({'latitude': lat, 'longitude': lon}, index=[0])
+    df['latitude'] = df['latitude'].apply(math.radians)
+    df['longitude'] = df['longitude'].apply(math.radians)
+    input = df[['latitude', 'longitude']]
+    
+    #load model from pickle
+    popdensity_model = pickle.load(open(PICKLE_FOLDER_PATH + 'popdensity_model.pkl', 'rb'))
+    
+    #predict with model
+    preds = popdensity_model.predict(input)
+    return preds[0]
