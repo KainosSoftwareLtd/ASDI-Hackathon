1130aae26f0f8c89cf5e363dbba98c061941d4a3
joshua.grefte <joshua.grefte@kainos.com>
2022-08-20 12:27:23 +0100

    added all functions to create final csv to helpers.py

--- Spikes/Dash/app.py
+++ Spikes/Dash/app.py
@@ -5,6 +5,7 @@ from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
+import plotly.figure_factory as ff
 
 app = Dash(__name__)
 
@@ -68,15 +69,23 @@ app.layout = html.Div(children=[
 def build_graph(column_chosen):
     # density
     #https://plotly.com/python/builtin-colorscales/
-    fig = px.density_mapbox(df, lat='Latitude', lon='Longitude', z=column_chosen, radius=15, opacity=0.5,
-                        center=dict(lat=51.50009, lon=0.1268072), zoom=10.1,
-                        #'open-street-map', 'carto-positron", 'carto-darkmatter', 'stamen-terrain', 'stamen-toner', 'stamen-watercolor' 
-                        mapbox_style="carto-positron",
-                        color_continuous_scale = 'Turbo')   #Thermal
+    # fig = px.density_mapbox(df, lat='Latitude', lon='Longitude', z=column_chosen, radius=15, opacity=0.5,
+    #                     center=dict(lat=51.50009, lon=0.1268072), zoom=10.1,
+    #                     #'open-street-map', 'carto-positron", 'carto-darkmatter', 'stamen-terrain', 'stamen-toner', 'stamen-watercolor' 
+    #                     mapbox_style="carto-positron",
+    #                     color_continuous_scale = 'Turbo')   #Thermal
     
     # scatter
     # fig = px.scatter_mapbox(df, lat='Latitude', lon='Longitude', opacity = 0.5,
-    #                         color = column_chosen, zoom=9.5, mapbox_style="carto-darkmatter")
+    #                         color = column_chosen, zoom=9.5, mapbox_style="carto-positron")
+    
+    # >100 horizontal hexagons has performance issues, long to load
+    fig = ff.create_hexbin_mapbox(df, lat="Latitude", lon="Longitude", color=column_chosen, nx_hexagon=100, 
+                                  opacity=0.3, center=dict(lat=51.50009, lon=0.1268072),
+                                  mapbox_style="carto-positron", zoom=10.1, color_continuous_scale = 'Turbo',
+                                  labels={"color": column_chosen})
+                                  #, min_count=1)
+    fig.update_traces(marker_line_width=0)
     
     fig.update_layout(
         autosize=True,
