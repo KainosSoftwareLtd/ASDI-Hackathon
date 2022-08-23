355f9207278b49d3c1fb1b52e2f893f5dc975038
joshua.grefte <joshua.grefte@kainos.com>
2022-08-23 13:12:47 +0100

    updated create_final.py to reflect changes in s3 bucket reading/writing functionality

--- Scripts/create_final_df.py
+++ Scripts/create_final_df.py
@@ -1,5 +1,5 @@
 from helpers import *
 
-final_df = fill_points_df()
+final_df = fill_points_df('asdi-hackathon', 'points_df.csv')
 
 upload_df_to_s3(bucket = 'asdi-hackathon', df = final_df, key = 'final-data/final_df.csv')
