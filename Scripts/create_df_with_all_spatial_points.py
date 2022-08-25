6054a3a9aef2e110881c1e9f37bc66002195989f
joshua.grefte <joshua.grefte@kainos.com>
2022-08-25 08:39:33 +0100

    updated Scripts

--- Scripts/create_df_with_all_spatial_points.py
+++ Scripts/create_df_with_all_spatial_points.py
@@ -2,4 +2,4 @@ from helpers import *
 
 points_df = get_spaced_point_set_in_bbox(0.25, (51.239405, -0.625211), (51.737184, 0.328289))
 
-upload_df_to_s3('asdi-hackathon', points_df, 'points_df.csv')
+upload_df_to_s3('asdi-hackathon', points_df, 'points_df_025.csv')
--- Scripts/create_final_df.py
+++ Scripts/create_final_df.py
@@ -1,5 +1,9 @@
 from helpers import *
 
-final_df = fill_points_df('asdi-hackathon', 'points_df.csv')
+penultimate_df = fill_points_land_type_df('asdi-hackathon', 'land_type_025.csv')
+    
+upload_df_to_s3(bucket = 'asdi-hackathon', df = penultimate_df, key = 'final-data/penultimate_df.csv')
+
+final_df = fill_penultimate_df('asdi-hackathon', 'final-data/penultimate_df.csv')
 
 upload_df_to_s3(bucket = 'asdi-hackathon', df = final_df, key = 'final-data/final_df.csv')
